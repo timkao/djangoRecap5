b928920625e99a83916156b2e942dcb906a780f0
Tim Kao <pakuya.kao@gmail.com>
2018-11-04 15:22:56 -0500

    add test cases

--- blog/tests.py
+++ blog/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 from .models import Post
 
@@ -24,8 +25,28 @@ class BlogTests(TestCase):
     self.assertEqual(str(post), post.title)
 
   def test_post_detail_view(self):
-    response = self.client.get('/post/1')
-    no_response = self.client.get('/post/10000')
+    response = self.client.get('/post/1/')
+    no_response = self.client.get('/post/10000/')
     self.assertEqual(response.status_code, 200)
     self.assertEqual(no_response.status_code, 404)
+
+  def test_post_create_view(self):
+    response = self.client.post(reverse('postNew'), {
+      'title': 'New title',
+      'author': self.user,
+      'body': 'New text',
+    })
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, 'New title')
+
+  def test_post_edit_view(self):
+    response = self.client.post(reverse('postEdit', args='1'), {
+      'title': 'Update title',
+      'body': 'Update text',
+    })
+    self.assertEqual(response.status_code, 302)
+
+  def test_post_delete_view(self):
+    response = self.client.get(reverse('postDelete', args='1'))
+    self.assertEqual(response.status_code, 200)
 
