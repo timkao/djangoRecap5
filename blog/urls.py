9d43ac5ad927827764cc8eb39dfd67968fef6c3d
Tim Kao <pakuya.kao@gmail.com>
2018-11-04 14:26:15 -0500

    added update view

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 urlpatterns = [
-  path('post/new', BlogCreateView.as_view(), name='postNew'),
-  path('post/<int:pk>', BlogDetailView.as_view(), name='postDetail'),
+  path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='postEdit'),
+  path('post/new/', BlogCreateView.as_view(), name='postNew'),
+  path('post/<int:pk>/', BlogDetailView.as_view(), name='postDetail'),
   path('', BlogListView.as_view(), name='home')
 ]
